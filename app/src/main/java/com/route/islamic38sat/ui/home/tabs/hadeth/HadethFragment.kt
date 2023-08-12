e222f4879c114ea9c559c0d0cfccbc2dfededa79
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2023-08-12 15:19:59 +0300

    + hadeth details + hadeth tab

--- app/src/main/java/com/route/islamic38sat/ui/home/tabs/hadeth/HadethFragment.kt
+++ app/src/main/java/com/route/islamic38sat/ui/home/tabs/hadeth/HadethFragment.kt
@@ -1,11 +1,15 @@
 package com.route.islamic38sat.ui.home.tabs.hadeth
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.route.islamic38sat.databinding.FragmentHadethBinding
+import com.route.islamic38sat.ui.Constants
+import com.route.islamic38sat.ui.hadethDetails.HadethDetailsActivity
+import com.route.islamic38sat.ui.model.Hadeth
 
 class HadethFragment : Fragment() {
     lateinit var viewBinding: FragmentHadethBinding
@@ -16,5 +20,49 @@ class HadethFragment : Fragment() {
     ): View? {
         viewBinding = FragmentHadethBinding.inflate(inflater, container, false)
         return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    lateinit var adapter: HadethRecyclerAdapter
+    private fun initViews() {
+        adapter = HadethRecyclerAdapter(null)
+        adapter.onItemClickListener = HadethRecyclerAdapter.OnItemClickListener { pos, hadeth ->
+            showHadethDetails(hadeth)
+        }
+        viewBinding.recyclerView.adapter = adapter
+    }
+
+    private fun showHadethDetails(hadeth: Hadeth) {
+        val intent = Intent(activity, HadethDetailsActivity::class.java)
+        intent.putExtra(Constants.EXTRA_HADETH, hadeth)
+        startActivity(intent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadHadethFile()
+        bindHadethList()
+    }
+
+    private fun bindHadethList() {
+        adapter.bindItems(hadethList)
+    }
+
+    val hadethList = mutableListOf<Hadeth>()
+    private fun loadHadethFile() {
+        val fileContent =
+            requireActivity().assets.open("ahadeth.txt").bufferedReader().use { it.readText() }
+        val singleHadethList = fileContent.trim().split("#")
+        singleHadethList.forEach { element ->
+            val lines = element.trim().split("\n")
+            val title = lines[0]
+            val content = lines.joinToString("\n")
+            val hadeth = Hadeth(title, content)
+            hadethList.add(hadeth)
+        }
     }
 }
